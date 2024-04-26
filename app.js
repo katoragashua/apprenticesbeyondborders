ef4b4ef6214faf2a0d72dff37a8ebc24ff75a2fb
Katoragashua <katoragashua@outlook.com>
2024-04-26 12:55:47 +0100

    ABB: Refactoring still

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import cors from "cors";
 import "express-async-errors";
 config();
 const router = Router();
-import path from "path";
 
 import mongoose from "mongoose";
 import Isemail from "isemail";
@@ -96,15 +95,10 @@ const getEmails = async (req, res) => {
 
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use("/", express.static("public"));
-
-
+app.use("/", express.static("./public"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile("/public/index.html");
-});
 app.post("/api/v1/users", saveUserEmail);
 app.get("/api/v1/users", getEmails);
 
